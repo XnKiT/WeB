c6fdbabde6651603eb92de08daea1c062579d7e6
⚝αикιт кυмαя⚝ <98248106+XnKiT@users.noreply.github.com>
2022-07-05 13:29:54 +0530

    Delete heroku.py

--- heroku.py
+++
@@ -1,9 +0,0 @@
-import os
-
-NAME = os.environ.get("NAME", default="AnKiT KuMaR")
-IMAGE = os.environ.get("IMAGE", default="https://te.legra.ph/file/1ee4f0185a4e85cfe689d.jpg")
-SHORT_BIO = os.environ.get("SHORT_BIO", default="A NooB")
-ABOUT = os.environ.get("ABOUT", default="Aɢᴀʀ Zɪɴᴅᴀɢɪ Aᴀᴘᴋᴏ Zᴀᴍᴇᴇɴ Pᴀʀ Gɪʀᴀ Dᴇ,Tᴏʜ Vᴀʜɪ Sᴏ Jᴀᴏ...🤪 ~ (c) XnKiT")
-GITHUB = os.environ.get("GITHUB", default=None)
-FACEBOOK = os.environ.get("FACEBOOK", default=None)
-INSTAGRAM = os.environ.get("INSTAGRAM", default=None)
